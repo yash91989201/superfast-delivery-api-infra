4b854a45a6a7277bff9a93b12d18fee9cbd3e30f
Yashraj Jaiswal <yashraj6370@gmail.com>
2025-02-27 08:16:07 +0000

    update the folder structure and neatly organize the files

---
+++ k8s/cluster/apps/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Create required namespaces
+kubectl create ns argocd
+kubectl create ns headlamp
+#
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+#
+helm repo add headlamp https://headlamp-k8s.github.io/headlamp
+#
+kubectl apply -f traefik-dashboard.yml
+#
+kubectl apply -f headlamp.yml
+#
+kubectl apply -f argocd.yml
+#
+kubectl create token headlamp --namespace headlamp --duration=0
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
--- k8s/cluster/setup/install.sh
+++ k8s/cluster/setup/install.sh
@@ -8,14 +8,10 @@ helm repo add jetstack https://charts.jetstack.io
 
 helm repo add traefik https://helm.traefik.io/traefik
 
-helm repo add headlamp https://headlamp-k8s.github.io/headlamp
-
 helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server
 
 # Create required namespaces
-kubectl create ns argocd
 kubectl create ns traefik
-kubectl create ns headlamp
 kubectl create ns cert-manager
 
 # Installations
@@ -29,22 +25,4 @@ helm upgrade -i traefik traefik/traefik -n traefik -f traefik/values.yml
 
 helm upgrade --install metrics-server metrics-server/metrics-server
 
-kubectl apply -f cluster-setup/clusterissuer-staging.yml
-
-# Steup traefik dashboard
-cd traefik-dashboard
-kubectl apply -f secret.yml -f middleware.yml -f certificate.yml -f ingressroute.yml
-
-# Steup headlamp
-cd ../headlamp
-kubectl apply -f certificate.yml -f ingressroute.yml
-# create headlamp token
-kubectl create token headlamp --namespace headlamp --duration=0
-
-# Steup argocd
-cd ../argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl apply -f middleware.yml -f certificate.yml -f ingressroute.yml
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
-
-cd ..
+kubectl apply -f cert-manager/clusterissuer-staging.yml

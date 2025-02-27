0c3f5ad30457c3d7466df0a4f170eae8ce3af2b9
Yashraj Jaiswal <yashraj6370@gmail.com>
2025-02-27 10:38:07 +0000

    update install scripts

--- k8s/cluster/apps/install.sh
+++ k8s/cluster/apps/install.sh
@@ -12,6 +12,8 @@ kubectl apply -f traefik-dashboard.yml
 kubectl apply -f headlamp.yml
 #
 kubectl apply -f argocd.yml
+
+sleep 1m
 #
 kubectl create token headlamp --namespace headlamp --duration=0
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
--- k8s/cluster/setup/install.sh
+++ k8s/cluster/setup/install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Create required namespaces
+kubectl create ns traefik
+kubectl create ns cert-manager
+
 kubectl apply -k "github.com/kubernetes-sigs/aws-ebs-csi-driver/deploy/kubernetes/overlays/stable/?ref=release-1.40"
 
 helm repo add external-dns https://kubernetes-sigs.github.io/external-dns
@@ -9,10 +13,6 @@ helm repo add jetstack https://charts.jetstack.io
 helm repo add traefik https://helm.traefik.io/traefik
 
 helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server
-
-# Create required namespaces
-kubectl create ns traefik
-kubectl create ns cert-manager
 
 # Installations
 helm upgrade --install external-dns external-dns/external-dns -n kube-system -f external-dns/values.yml
